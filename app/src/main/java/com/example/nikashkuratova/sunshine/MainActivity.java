29186d99d2b5334b8270291acf1b0fdc6480e099
nshkuratova <nika.shkuratova@gmail.com>
2016-06-24 00:01:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nikashkuratova/sunshine/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.nikashkuratova.sunshine;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    public static  class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment(){}
+
+            @Override
+            public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+                View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                return rootView;
+        }
+    }
+}
